61d3036ba71ed7414f82d8e7e4e3f9e31916ed43
root <root@DESKTOP-N4BIA51>
2024-01-01 23:41:25 +0300

    Add an auto cover tasks to import shared task from multipule apps.

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -7,3 +7,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
 app = Celery("dcelery")
 
 app.conf.from_object("django.conf:settings", namespace="CELERY")
+
+@app.task
+def add_numbers():
+    return
+
+app.autodiscover_tasks()
