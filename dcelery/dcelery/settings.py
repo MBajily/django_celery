f20a62c99de4c1e49dbe0a916b115130d6a34b55
Mohammed Elgaily <MBajily@gmail.com>
2024-01-07 00:05:53 +0300

    Update settings.py file.

--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -112,4 +112,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CELERY
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_ACKEND", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
